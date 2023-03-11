1563fb865a8bc89b0ecd9555bd75ad47ef745489
= <tominoboda@gmail.com>
2023-03-11 23:43:27 +0100

    feat(global): Added support for global variables

--- src/app/App.h
+++ src/app/App.h
@@ -1,6 +1,7 @@
 #include <wx/wx.h>
 #include "../window/Window.h"
 #include "../audio/AudioPlayer.h"
+#include "../config.h"
 
 #ifndef FFT_MUSIC_VISUALISER_APP_H
 #define FFT_MUSIC_VISUALISER_APP_H
@@ -13,11 +14,10 @@ private:
 };
 
 bool App::OnInit() {
-    window = new Window("Music Visualiser", wxPoint(50, 50), wxSize(800, 500));
+    window = new Window("Music Visualiser", wxPoint(50, 50), wxSize(constants::WIDTH, constants::HEIGHT));
 
     AudioPlayer audio_player;
-    const char * filename = "../never-gonna-give-u-up.wav";
-    audio_player.play_audio(filename);
+    audio_player.play_audio(constants::FILENAME);
 
     return true;
 }
---
+++ src/config.h
@@ -0,0 +1,8 @@
+
+namespace constants {
+
+    const int WIDTH = 800;
+    const int HEIGHT = 500;
+
+    const char * FILENAME = "../never-gonna-give-u-up.wav";
+}
