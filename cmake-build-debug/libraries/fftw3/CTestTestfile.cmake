6a3119e6e69f7ec9a15a7b06b538ba5e58a4dfcb
= <tominoboda@gmail.com>
2023-03-29 17:52:35 +0200

    chore(gitignore): Removed cmake-build-debug folder from git repository

--- cmake-build-debug/libraries/fftw3/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tomasboda/Desktop/music-visualiser-master/libraries/fftw3
-# Build directory: /Users/tomasboda/Desktop/music-visualiser-master/cmake-build-debug/libraries/fftw3
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
